f96583573e01c9041965b255c20b41c46190e39f
Priyankaadev <priyaaswal79@gmail.com>
2024-05-08 19:39:10 +0530

    integrated get categories on the homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,29 @@
+import axios from 'axios';
 import CategoryItem from '../../components/CategoryItem/CategoryItem';
 import './Home.css'
+import { useEffect, useState } from 'react';
+import { getAllCategories } from '../../apis/fakeStoreProdApis';
 
 function Home() {
+    const [categories, setCategories] = useState(null)
+    async function downloadCategories() {
+        const response = await axios.get(getAllCategories);
+        setCategories(response.data)
+    }
+
+    useEffect(() => {
+        downloadCategories();
+    }, [])
+
     return (
-        <div class="container welcome-wrapper"  style={{minHeight:'100vh', display:"flex", flexDirection:"column"}}>
+        <div class="container welcome-wrapper" style={{ minHeight: '100vh', display: "flex", flexDirection: "column" }}>
             <div class="row">
                 <h2 class="home-title text-center">Welcome to Shop Cart</h2>
                 <div class="category-list d-flex flex-row justify-content-between align-items-center" id="categoryList">
 
                     <CategoryItem itemName='All products' />
 
-
+                    {categories && categories.map(category => <CategoryItem itemName={category} key={category} />)}
                 </div>
                 <div class="category-title text-center">
                     Select a Category to Start Shopping
